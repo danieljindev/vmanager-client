160f75e4ef4c913333552b96f23a8430b830e96b
Daniel Jin <danieljin123@outlook.com>
2021-05-31 03:20:28 +0300

    add secret

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,12 +10,12 @@ const CONFIG = {
   http: {
     baseURL: isDevelopment
       ? 'http://localhost:7003/api'
-      : 'https://work-api.xiejiahe.com/api'
+      : 'https://vmanager-client.ingeniousvision.net/api'
   },
   github: {
-    clientId: isProduction ? '789d87c19dd5ed1dc42e' : '489b39e1f91d934128c8',
+    clientId: isProduction ? 'c82d09795998a4a4d0f1' : 'c82d09795998a4a4d0f1',
     // callbackURL 不可随意更改, 否则需要与服务端配置文件一同修改
-    callbackURL: `${isProduction ? 'https://work-api.xiejiahe.com' : window.location.origin}/api/passport/github/callback`,
+    callbackURL: `${isProduction ? 'https://vmanager-client.ingeniousvision.net' : window.location.origin}/api/passport/github/callback`,
     repositoryUrl: 'https://github.com/danieljindev/vmanager-client',
     bug: 'https://github.com/danieljindev/vmanager-client/issues'
   }
